cc2e41d333883d6afee488fe8da0360f9649db09
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-21 18:52:38 -0800

    removal of swapped variable

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,50 +2,44 @@
 #include <stdbool.h>
 
 /**
-* swap - Swaps the values of two integers
-*
-* @xp: Pointer to the first integer
-* @yp: Pointer to the second integer
-*/
+ * swap - Swaps the values of two integers
+ *
+ * @xp: Pointer to the first integer
+ * @yp: Pointer to the second integer
+ */
 void swap(int *xp, int *yp)
 {
-int temp = *xp;
-*xp = *yp;
-*yp = temp;
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
 
 /**
-* bubble_sort - Sorts an array of integers in ascending order using the
-*               Bubble Sort algorithm and prints the array after each pass.
-*
-* @array: The array to be sorted
-* @size: Number of elements in the array
-*/
+ * bubble_sort - Sorts an array of integers in ascending order using the
+ *               Bubble Sort algorithm and prints the array after each pass.
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int swapped;
+    size_t i, j;
 
-for (i = 0; i < size - 1; i++)
-{
-swapped = 0;
+    if (array == NULL || size <= 0)
+        return;
 
-for (j = 0; j < size - i - 1; j++)
-{
+    for (i = 0; i < size - 1; i++)
+    {
 
-if (array[j] > array[j + 1])
-{
-swap(&array[j], &array[j + 1]);
-swapped = true;
-print_array(array, size);
-}
-/*print_array(array, size);*/
-}
-/* print_array(array, size);*/
+        for (j = 0; j < size - i - 1; j++)
+        {
 
+            if (array[j] > array[j + 1])
+            {
+                swap(&array[j], &array[j + 1]);
+                print_array(array, size);
+            }
+        }
 
-/* If no two elements are swapped by inner loop, the break */
-if (!swapped)
-break;
-}
+    }
 }
