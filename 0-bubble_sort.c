f5d6d1c16b5832d7c11040b008fa06e416c1e782
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-21 18:34:29 -0800

    task 0 - bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,51 @@
 #include "sort.h"
+#include <stdbool.h>
 
-void swap(int* xp, int* yp)
+/**
+* swap - Swaps the values of two integers
+*
+* @xp: Pointer to the first integer
+* @yp: Pointer to the second integer
+*/
+void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
 }
 
-void bubbleSort(int arr[], int n)
+/**
+* bubble_sort - Sorts an array of integers in ascending order using the
+*               Bubble Sort algorithm and prints the array after each pass.
+*
+* @array: The array to be sorted
+* @size: Number of elements in the array
+*/
+void bubble_sort(int *array, size_t size)
 {
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i+)
-    {
-        swapped = false;
-        for (j = 0; j < n - i -j; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
-            }
-        }
-        /* If no two elements are swapped by inner loop, the break */
-        if (swapped == false)
-            break;
-    }
+size_t i, j;
+int swapped;
+
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
+
+for (j = 0; j < size - i - 1; j++)
+{
+
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+swapped = true;
+print_array(array, size);
+}
+/*print_array(array, size);*/
+}
+/* print_array(array, size);*/
+
+
+/* If no two elements are swapped by inner loop, the break */
+if (!swapped)
+break;
+}
 }
--- sort.h
+++ sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,6 +18,9 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
