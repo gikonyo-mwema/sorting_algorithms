beca6866f5b14b7e36e6f188f1381dd0c33a91a2
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-22 02:54:06 -0800

    insertion betty pass

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,47 @@
 #include "sort.h"
-
-void insertion_sort_list(listint_t** list)
+/**
+* insertion_sort_list - performs a swap operation every time
+*                       it finds an element that is smaller that prev
+* @list: pointer to pointe of list
+* Return: nothing
+**/
+void insertion_sort_list(listint_t **list)
 {
 
-    listint_t* current;
-    listint_t* temp;
+listint_t *current;
+listint_t *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-    {
-        return;
-    }
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+{
+return;
+}
 
-    current = (*list)->next;
+current = (*list)->next;
 
-    while (current != NULL)
-    {
-        temp = current;
-        while (temp != *list && temp->n < temp->prev->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next != NULL)
-            {
-                temp->next->prev = temp->prev;
-            }
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
-            if (temp->prev != NULL)
-            {
-                temp->prev->next = temp;
-            }
-            else
-            {
-                *list = temp;
-            }
-            print_list(*list);
-        }
-        current = current->next;
-    }
+while (current != NULL)
+{
+temp = current;
+while (temp != *list && temp->n < temp->prev->n)
+{
+temp->prev->next = temp->next;
+if (temp->next != NULL)
+{
+temp->next->prev = temp->prev;
+}
+temp->next = temp->prev;
+temp->prev = temp->prev->prev;
+temp->next->prev = temp;
+if (temp->prev != NULL)
+{
+temp->prev->next = temp;
+}
+else
+{
+*list = temp;
+}
+print_list(*list);
+}
+current = current->next;
+}
 }
 
