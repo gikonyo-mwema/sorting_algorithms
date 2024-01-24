609b1f2a0cfa8e3eba0d350aa9acc77e90296912
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-23 17:33:48 -0800

    null check adddition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,43 @@
 #include "sort.h"
 
 /**
- * swap_int - Swaps the values of two integers
- * @a: The first integer to be swapped
- * @b: The second integer to be swapped
- */
+* swap_int - Swaps the values of two integers
+* @a: The first integer to be swapped
+* @b: The second integer to be swapped
+*/
 void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+int temp = *a;
+
+*a = *b;
+*b = temp;
 }
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
+* selection_sort - Sorts an array of integers in ascending order
+* @array: The array to be sorted
+* @size: Number of elements in the array
+*/
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+size_t i, j, min_idx;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+if (array == NULL || size < 2)
+return;
 
-        if (min_idx != i)
-        {
-            swap_int(&array[min_idx], &array[i]);
-            print_array(array, size);
-        }
-    }
+for (i = 0; i < size - 1; i++)
+{
+min_idx = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_idx])
+min_idx = j;
+}
+
+if (min_idx != i)
+{
+swap_int(&array[min_idx], &array[i]);
+print_array(array, size);
+}
+}
 }
